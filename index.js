6b36e0579636e2ae2c48734bd4a9c0efd5a350f9
JStiiven <johnortega13579@gmail.com>
2024-02-15 08:04:08 -0500

    Se crea variable questionAge y se hace validación si es dato numerico

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ let petQuesition = prompt("Qué mascota desea saber sobre su ejercicio? \n- Perr
 
 if (petQuesition !== "Perro" && petQuesition !== "Gato" && petQuesition !== "Ave") {
     alert("Tipo de mascota inválida");
+} else {
+    let questionAge = parseInt(prompt("Qué edad tiene su mascota?"));
+    if (isNaN(questionAge)) {
+        alert("Por favor, ingrese una edad válida.");
+    }
 }
