4dc5e466fda42ef10b8d5a8927655f9111668409
JStiiven <johnortega13579@gmail.com>
2024-02-15 08:19:11 -0500

    Se hace las condicionales de la mascota perro con sus respectivas edades

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ if (petQuesition !== "Perro" && petQuesition !== "Gato" && petQuesition !== "Ave
     let questionAge = parseInt(prompt("Qué edad tiene su mascota?"));
     if (isNaN(questionAge)) {
         alert("Por favor, ingrese una edad válida.");
-    }else {
+    } else {
         getPetExerciseInfo(petQuesition, questionAge);
     }
 }
 
 function getPetExerciseInfo(pet, age) {
+    if (pet === "Perro") {
+        if (age < 1) {
+            alert("Los cachorros necesitan pequeñas y frecuentes sesiones de juego")
+        } else if (age > 1 || age < 7) {
+            alert("Los perros a esta age necesitan caminatas diarias y sesiones de juego")
+        } else if (age >= 7) {
+            alert("Los perros viejos necesitan caminatas más cortas")
+        }
 
+    }
 }
